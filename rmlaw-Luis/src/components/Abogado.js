e383bfb128e1a0c5582d6eebf5b3cfd1a27fe818
Luis Osvaldo <aldolp40@gmail.com>
2019-06-25 14:54:08 -0500

    change attorneys lang

--- rmlaw-Luis/src/components/Abogado.js
+++ rmlaw-Luis/src/components/Abogado.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import '../css/MainContent.css';
 
 const Abogado = props => {
     return(
         <div>
-            <a className='lawyer' href={props.abogadoPage}>
+            <Link className='lawyer' to={props.abogadoPage}>
                 <p style={{fontSize:17 ,color:"#000"}}>{props.Nombre}</p>
                 <p style={{fontSize:13, color:"#1b8ab4"}} className="font">{props.Rol}</p>
                 <p style={{fontSize:15, color:"#1b8ab4"}} className="font">{props.data.contenido.abogados.sintesis}</p>
-            </a>
+            </Link>
         </div>
     )
 }
